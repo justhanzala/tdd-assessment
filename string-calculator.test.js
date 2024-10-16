ce1c5a21e4d5cb07d1bd546a75233bea50553e5c
justhanzala <justhanzala786@gmail.com>
2024-10-16 12:36:41 +0530

    Handle more than two comma-separated numbers

--- string-calculator.test.js
+++ string-calculator.test.js
@@ -12,4 +12,8 @@ describe("String Calculator", () => {
   test("should return the sum of two comma-separated numbers", () => {
     expect(add("1,2")).toBe(3);
   });
+
+  test("should return the sum of multiple comma-separated numbers", () => {
+    expect(add("1,2,3,4")).toBe(10);
+  });
 });
